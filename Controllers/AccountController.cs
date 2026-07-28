80d62a03557c0c045001c8e3f9491f5aad84492e
Althea Vallejos <teyakuromi.99@gmail.com>
2026-07-28 18:48:54 +0800

    add to repo

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vallejos_MidtermProject_BSIT31E2.Models;
+
+namespace Vallejos_MidtermProject_BSIT31E2.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (model.Username == "admin" && model.Password == "123456")
+            {
+                return RedirectToAction("Welcome");
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid username or password.");
+            return View(model);
+        }
+
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vallejos_MidtermProject_BSIT31E2.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters.")]
+        public string Password { get; set; }
+    }
+}
